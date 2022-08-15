e3682db8fa28b16604c85b33eeace0c6f9af7b94
MehdiSaadati <108065298+MahdiSaadatiOrg@users.noreply.github.com>
2022-08-15 12:29:40 +0430

    Add files via upload

---
+++ QueueLinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using QueueLinkedList;
+
+
+QueueImplementLinkedList a = new QueueImplementLinkedList();
+
+a.EnQueue(1);
+a.EnQueue(2);
+a.EnQueue(3);
+a.EnQueue(4);
+a.EnQueue(5);
+
+a.DeQueue();
+a.DeQueue();
+//a.DeQueue();
+//a.DeQueue();
+//a.DeQueue();
+//a.DeQueue();
+//a.DeQueue();
+//a.DeQueue();
+//a.DeQueue();
+//a.DeQueue();
+//a.DeQueue();
+
+
+a.Print();
